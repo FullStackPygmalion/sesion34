28a45c6697867b464b5bdfb7986ce249948eb77c
Juan Giovanni Restrepo <grestrepojimenez@gmail.com>
2020-07-02 22:58:46 -0500

    Uso de conditional Render, Get data y uso de hooks en React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import UsoHookState from './components/usoHookState'
+import CambiarTitulo from './components/CambiarTitulo'
+
+// let isLogoedIn = true;
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <React.Fragment >
+            <UsoHookState/>
+            <hr />
+            <CambiarTitulo/>
+        </React.Fragment >
+    )
 }
 
 export default App;
